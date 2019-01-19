5627a76de0583fa17ec6ee639640a337a112f5bc
Deep_Kapadia <kapadia.deep12@gmail.com>
2019-01-19 19:18:13 +0530

    IQKeayboard_Avoiding_WithPOD_Swift
    IQKeayboard_Avoiding_WithPOD_Swift

---
+++ IQKeyboardManager_Avoiding_Swift4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IQKeyboardManager_Avoiding_Swift4
+//
+//  Created by DeEp KapaDia on 20/06/18.
+//  Copyright © 2018 DeEp KapaDia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-IQKeyboardManager_Avoiding_Swift4/Pods-IQKeyboardManager_Avoiding_Swift4-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IQKeyboardManager_Avoiding_Swift4 : NSObject
+@end
+@implementation PodsDummy_Pods_IQKeyboardManager_Avoiding_Swift4
+@end
